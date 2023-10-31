f0d081b8c8c1dc12caf39c754b55327581d71ddf
TezzBhandari <bhandaritezz18@gmail.com>
2023-10-31 08:44:17 +0545

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Customers = () => {
+  console.log("cusomer renders");
+
+  return <div>Customers page</div>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Invoices = () => {
+  console.log("invoice renders");
+  return <div>Invoices page</div>;
+};
+
+export default Invoices;
